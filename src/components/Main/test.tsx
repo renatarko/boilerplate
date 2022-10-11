9218089533972d4ffcbce0665b4700f750571272
Renata Karonlina <renatakarolinarko@gmail.com>
2022-10-11 10:55:41 -0400

    Add RTL config and firts test

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,12 +1,15 @@
 import { screen, render } from "@testing-library/react";
 import Main from ".";
 
+// describe: o que estamos testando
 describe("<Main />", () => {
   it("should render the heading", () => {
-    render(<Main />);
+    const { container } = render(<Main />);
 
     expect(
       screen.getByRole("heading", { name: /React Avançado/i })
     ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
