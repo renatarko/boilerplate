793f18dfcf74507cb803bd301232c30902a54ec4
Renata Karonlina <renatakarolinarko@gmail.com>
2022-10-11 10:02:51 -0400

    Configurações aplicadas.

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,12 @@
+import { screen, render } from "@testing-library/react";
+import Main from ".";
+
+describe("<Main />", () => {
+  it("should render the heading", () => {
+    render(<Main />);
+
+    expect(
+      screen.getByRole("heading", { name: /React Avançado/i })
+    ).toBeInTheDocument();
+  });
+});
